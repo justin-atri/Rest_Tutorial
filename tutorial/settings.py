a2a01500ae6f255cce4867d5ffd7bc5d013a288e
Justin K. Jang <justinj@atrihub.io>
2022-09-20 14:02:59 -0700

    feat(training): Rest Tutorial
    Create local settings file.
    (I assume they should not be included in VCS, by using .gitignore, but for training purpose I included them)
    
    [ICTRAINING-1220]

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +126,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+from .settings_local import *
